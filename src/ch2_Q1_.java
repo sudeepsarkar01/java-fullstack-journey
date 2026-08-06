57b4d863343cf51a44a1b55750d9e658278b367e
sudeepsarkar01 <sudeepsarkar276@gmail.com>
2026-08-06 19:15:15 +0530

    add new file

---
+++ src/ch2_Q1_.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ch2_Q1_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        float num1 = sc.nextFloat();
+        System.out.print("Enter another number: ");
+        float num2 = sc.nextFloat();
+        if (num1 > num2) {
+            System.out.println("num1 is greater than num2.");
+        } else {
+            System.out.println("num2 is greater than num1.");
+        }
+    }
+}
---
+++ src/ch2_increment_decrement_operator.java
@@ -0,0 +1,9 @@
+public class ch2_increment_decrement_operator {
+    public static void main(String[] args) {
+        int x = 7;
+//        System.out.println(++x);
+        int y = ++x * 8;
+        System.out.println(y);
+        System.out.println(x);
+    }
+}
