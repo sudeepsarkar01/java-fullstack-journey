8d834f10c02f3589bb3b7c6ce8fe47e91d2bb9bf
sudeepsarkar01 <sudeepsarkar276@gmail.com>
2026-08-07 18:17:21 +0530

    Add string file

---
+++ src/ch3_strings.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ch3_strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String s = new String("Sudeep Sarkar");
+        // String st = "Sudeep Sarkar";
+        // System.out.println(s);
+        // System.out.println(st);
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
