0e2351bee07584d74027005b89dfea32ffdc8047
sudeepsarkar01 <sudeepsarkar276@gmail.com>
2026-08-07 19:53:33 +0530

    add string methods

---
+++ src/ch3_string_methods.java
@@ -0,0 +1,32 @@
+public class ch3_string_methods {
+    static void main(String[] args) {
+        String name = "Harry";
+        System.out.println(name);
+        System.out.println(name.length());
+
+        String lower = name.toLowerCase();
+        System.out.println(lower.toLowerCase());
+
+        String upper = name.toUpperCase();
+        System.out.println(upper);
+
+        String trim = "  Sudeep Sarkar  ";
+        String newtrim = trim.trim();
+        System.out.println(newtrim);
+        System.out.println(trim);
+
+        System.out.println(name.substring(2));
+        System.out.println(name.substring(0,5));
+
+        System.out.println(name.replace('r' , 'p'));
+        System.out.println(name.replace("r" , "pp"));
+
+        System.out.println(name.startsWith("ha"));
+        System.out.println(name.endsWith("y"));
+
+        System.out.println(name.charAt(4));
+
+        System.out.println(name.indexOf("y"));
+        System.out.println(name.indexOf("H" , 3));
+    }
+}
